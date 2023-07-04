8ec7cfdced59fea3ece216785a01b8034aab95b7
Eisson <eisson.alipio@gmail.com>
2023-07-04 00:01:26 -0500

    chore: addings cors

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import express, { type Request, type Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import { uploadMulter, uploadB2 } from './middleware';
 
 dotenv.config();
 const app = express();
 
+app.use(cors());
 // routes
 app.post('/upload', uploadMulter, uploadB2, (req: Request, res: Response) => {
-  res.json(res.locals);
+  res.json(res.locals.url);
 });
 
 app.listen(process.env.PORT, () => {
