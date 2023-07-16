84d279d3aad321cf4b9d41e8289ebc994e03d3a2
Eisson <eisson.alipio@gmail.com>
2023-07-16 09:29:38 -0500

    style: split componentes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,10 @@
 import { Form } from "./components/form";
+import { Header } from "./components/header";
 
 function App() {
   return (
     <>
-      <header>
-        <div className="absolute left-0 right-0 top-2 z-40 pt-14">
-          <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <div className="mx-auto max-w-2xl lg:max-w-none">
-              <div className="flex items-center justify-between">
-                <span>Logo</span>
-                <span>menu</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </header>
-
+      <Header />
       <div className="relative flex flex-auto bg-white pt-14">
         <div className="relative isolate flex w-full flex-col pt-9">
           <main className="w-full flex-auto">
